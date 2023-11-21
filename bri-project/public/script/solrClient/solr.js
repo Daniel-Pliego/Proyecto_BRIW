69aad212bfdf2ddca9fbfdfed507297d89b5ccdb
Edwin-Lines <alonsoandrade28@hotmail.com>
2023-11-20 23:57:46 -0600

    Boolean search ready

--- bri-project/public/script/solrClient/solr.js
+++ bri-project/public/script/solrClient/solr.js
@@ -7,11 +7,11 @@ const solrClient = solr.createClient({
   path: '/solr',
 });
 
-function returnURLcore(){
+function returnURLcore() {
   return 'http://localhost:8983/solr/Prueba10';
 }
 
-function returnSolrClient(){
+function returnSolrClient() {
   return solrClient;
 }
 
