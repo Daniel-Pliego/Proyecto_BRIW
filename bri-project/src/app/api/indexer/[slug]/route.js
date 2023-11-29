16af84ed9410d1e847fd0dd1ee5135ab29fda529
Jonatan <jsantanamay@gmail.com>
2023-11-29 02:23:30 -0600

    add function to index file

--- bri-project/src/app/api/indexer/[slug]/route.js
+++ bri-project/src/app/api/indexer/[slug]/route.js
@@ -4,6 +4,9 @@
 import {initCrawler} from "public/script/crawler/crawler.js";
 import fsPromises from 'fs/promises';
 import path from 'path';
+import axios from 'axios';
+const FormData = require('form-data');
+const fs = require('fs');
 
 const dataFilePath = path.join(process.cwd(), 'json/urlbase.json');
 
@@ -38,4 +41,24 @@ export async function POST(request, { params }) {
         console.error(error);
         return Response.json({ message: "Error al actualizar los datos" });
       }
+  }
+
+  async function enviarArchivoASolr() {
+    const filePath = 'C:\\Users\\jonat\\OneDrive\\Documentos\\7toSemestre\\busquedaWeb\\solr-8.11.2\\example\\exampledocs\\Welcome.pdf';
+    const solrUrl = 'http://localhost:8983/solr/v10/update/extract?literal.id=doc920&commit=true';
+    
+    const formData = new FormData();
+    formData.append('myfile', fs.createReadStream(filePath));
+    
+    axios.post(solrUrl, formData, {
+      headers: {
+        ...formData.getHeaders(),
+      },
+    })
+      .then(response => {
+        console.log('Respuesta de Solr:', response.data);
+      })
+      .catch(error => {
+        console.error('Error al enviar el archivo a Solr:', error);
+      });
   }
