90e582e43fcff3cfae14677b56ce88f5480e653a
Daniel-Pliego <a20216405@alumnos.uady.mx>
2023-12-05 19:47:28 -0600

    add: added autocomplete

--- bri-project/src/app/components/SearchSuggestions.jsx
+++ bri-project/src/app/components/SearchSuggestions.jsx
@@ -1,21 +1,22 @@
-"use client";
 import React from "react";
 
-export function SearchSuggestions() {
+export function SearchSuggestions({ suggestions, onSelectSuggestion }) {
   return (
-    <li className="flex flex-col py-2 list-none">
-      {[...Array(5)].map((_, index) => (
-        <ul
+    <ul className="flex flex-col py-2 list-none">
+      {suggestions.map((suggestion, index) => (
+        <li
           key={index}
           className="flex items-center py-1 rounded hover:bg-gray-100"
-          onClick={() => {}}
+          onClick={() => {
+            onSelectSuggestion(suggestion.word);
+          }}
         >
           <span className="material-symbols-outlined text-[20px] px-2">
             search
           </span>
-          Hola
-        </ul>
+          {suggestion.word}
+        </li>
       ))}
-    </li>
+    </ul>
   );
 }
--- bri-project/src/app/page.jsx
+++ bri-project/src/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 import ResultCard from "./components/ResultCard";
@@ -20,8 +20,18 @@ export default function Home() {
     setSearchQuery(query);
   };
 
-  const handleSubmit = async () => {
-    await axios
+  useEffect(() => {
+    if (searchQuery.length >= 3) {
+      axios
+        .get(`https://api.datamuse.com/sug?max=6&s=${searchQuery}`)
+        .then((res) => {
+          setSuggestions(res.data);
+        });
+    }
+  }, [searchQuery]);
+
+  const handleSubmit = () => {
+    axios
       .get(`/api/searcher/search?query=${searchQuery}`)
       .then((res) => {
         setData(res.data);
@@ -69,23 +79,42 @@ export default function Home() {
               />
             </div>
             {searchQuery.length > 0 && (
-              <SearchSuggestions suggestions={suggestions} />
+              <SearchSuggestions
+                {...{
+                  suggestions,
+                  onSelectSuggestion: (suggestion) => {
+                    setSearchQuery(suggestion);
+                    handleSubmit();
+                  },
+                }}
+              />
             )}
           </div>
         </form>
         <div className="flex justify-center w-full">
           <div className="flex flex-col w-full gap-2">
-            {data.response?.docs.map((doc) => {
-              if (filter.filterWord.length > 0) {
-                if (filter.type === "category") {
-                  if (doc.category.includes(filter.filterWord))
-                    return <ResultCard {...{ data: doc }} />;
-                } else if (filter.type === "domain") {
-                  if (new URL(doc.url).hostname === filter.filterWord)
-                    return <ResultCard {...{ data: doc }} />;
-                }
-              } else return <ResultCard {...{ data: doc }} />;
-            })}
+            {data.response?.docs.length === 0 ? (
+              <div className="flex flex-col items-center justify-center w-full bg-white border-2 border-indigo-300 h-96 rounded-3xl">
+                <span className="material-symbols-outlined text-[100px] text-indigo-300">
+                  search
+                </span>
+                <p className="text-2xl text-indigo-300">
+                  No se encontraron resultados
+                </p>
+              </div>
+            ) : (
+              data.response?.docs.map((doc) => {
+                if (filter.filterWord.length > 0) {
+                  if (filter.type === "category") {
+                    if (doc.category.includes(filter.filterWord))
+                      return <ResultCard {...{ data: doc }} />;
+                  } else if (filter.type === "domain") {
+                    if (new URL(doc.url).hostname === filter.filterWord)
+                      return <ResultCard {...{ data: doc }} />;
+                  }
+                } else return <ResultCard {...{ data: doc }} />;
+              })
+            )}
           </div>
         </div>
       </div>
