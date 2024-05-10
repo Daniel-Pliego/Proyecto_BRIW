ee179caa4c2ca9f0259c1485ca614220d1eca042
Emiliano <evalencia142000@gmail.com>
2024-05-10 16:00:12 -0600

    fix: despliegue correcto de perfiles de busqueda y nuevo query (CA-001)

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,11 +1,17 @@
 "use client";
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect  } from "react";
+import SearchProfileTable  from "../components/SearchProfileTable";
 
 export default function SearchProfiles(){
     const [profiles, setProfiles] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const firstQuery = "SELECT * FROM briw.urls_consulta WHERE perfil_busqueda_id IN (SELECT id FROM briw.perfiles_busqueda WHERE usuario_id = 1) order by perfil_busqueda_id;";
-    
+    //const firstQuery = "SELECT * FROM briw.urls_consulta WHERE perfil_busqueda_id IN (SELECT id FROM briw.perfiles_busqueda WHERE usuario_id = 1) order by perfil_busqueda_id;";
+    const firstQuery = "SELECT urls.*, perfiles.nombre AS nombre_perfil_busqueda " +
+                        "FROM urls_consulta AS urls " +
+                        "JOIN perfiles_busqueda AS perfiles ON perfil_busqueda_id = perfiles.id " +
+                        "WHERE perfiles.usuario_id = 1 " +
+                        "ORDER BY urls.perfil_busqueda_id;";
 
     useEffect(() => {
         const fetchData = async () => {
@@ -17,8 +23,8 @@ export default function SearchProfiles(){
                 });
                 if (response.ok) {
                     const data = await response.json();
-                    console.log("Usuarios:", data.usuarios);
                     setProfiles(data.usuarios);
+                    setLoading(false); // Cuando los perfiles se cargan, setLoading se establece en false
                 }
             } catch (error) {
                 alert("Error al obtener perfiles de búsqueda: " + error);
@@ -28,21 +34,28 @@ export default function SearchProfiles(){
         fetchData();
     }, []);
 
+    if (loading) {
+        return <div>Cargando perfiles...</div>; 
+    }
 
     return (
         <div className="container p-8 mx-auto mt-8 border border-gray-300 rounded-lg">
             <h1 className="mb-4 text-2xl font-bold">PERFILES DE BUSQUEDA</h1>
-            <ul>
-                {profiles.map(profile => (
-                    <li key={profile.id}>
-                        <p>Perfil de busqueda: {profile.perfil_busqueda_id}</p>
-                        <p>Nombre: {profile.nombre}</p>
-                        <p>url: {profile.url}</p>
-                        <p>Frecuencia de consulta: {profile.frecuencia_consulta_hrs || "0 hrs"}</p>
-                        {/* <p>Fecha de creación: {new Date(profile.fecha_creacion).toLocaleString()}</p> */}
-                    </li>
-                ))}
-            </ul>
+            <SearchProfileTable profiles={ profiles } />
+            {/* <ul> */}
+            {
+                //profiles.map(profile => (
+                    // <li key={profile.id}>
+                    //     <p>Perfil de busqueda: {profile.perfil_busqueda_id}</p>
+                    //     <p>Nombre: {profile.nombre}</p>
+                    //     <p>url: {profile.url}</p>
+                    //     <p>Frecuencia de consulta: {profile.frecuencia_consulta_hrs || "0 hrs"}</p>
+                    //     {/* <p>Fecha de creación: {new Date(profile.fecha_creacion).toLocaleString()}</p> */}
+                    // </li>
+                //))
+            }
+            {/* </ul> */}
+            {/* <SearchProfileTable /> */}
         </div>
     );
 };
