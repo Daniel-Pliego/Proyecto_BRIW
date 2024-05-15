27ab6c9eb4e267bc417fde2fd7cd428cadaf6496
Jonatan <jsantanamay@gmail.com>
2024-05-15 15:19:43 -0600

    feat: agregando core e infra (CA-008)

--- src/core/utils/searcher/expander.js
+++ src/core/utils/searcher/expander.js
@@ -1,5 +1,5 @@
 
-async function returnSynonyms(word, language) {
+async function returnSynonyms (word, language) {
     const response = await fetch(`http://127.0.0.1:8000/synonyms?word=${word}&language=${language}`);
     let data = null
     if (response.ok) {
--- src/infra/gateway/solr/solr.js
+++ src/infra/gateway/solr/solr.js
@@ -1,21 +1,23 @@
 const solr = require('solr-client');
 import { v4 as uuidv4 } from 'uuid';
 
-const nucleoname = 'v2';
+const coreName = process.env.CORE_NAME;
+const solrHost = process.env.SOLR_HOST;
+const solrPort = process.env.SOLR_PORT;
 
 const solrClient = solr.createClient({
-  host: 'localhost',
+  host: solrHost,
   port: 8983,
-  core: nucleoname,
+  core: solrPort,
   path: '/solr',
 });
 
 function returnURLcore () {
-  return 'http://localhost:8983/solr/'+nucleoname;
+  return 'http://localhost:8983/solr/'+coreName;
 }
 
 function returnURLFileIndexer (){
-  return `http://localhost:8983/solr/${nucleoname}/update/extract?literal.id=${uuidv4()}&commit=true`;
+  return `http://localhost:8983/solr/${coreName}/update/extract?literal.id=${uuidv4()}&commit=true`;
 }
 
 function returnSolrClient () {
