ef1e46bf627fb4a07d8eed93ead7794c75b3242c
Horacio Jahir Gómez Gómez <86177245+JahirGomez@users.noreply.github.com>
2024-05-17 09:50:56 -0600

    feat: agregando tests y exportando funciones (CA-004)

--- src/app/api/searcher/[slug]/route.test.js
+++ src/app/api/searcher/[slug]/route.test.js
@@ -1,6 +1,4 @@
-
-import { GET, buildQuery} from './route.js';
-
+import { GET, buildQuery, isAcceptedOperator, addSearchingOperator, generateSimilarWordsQuery} from './route.js';
 class FormDataMock {
     constructor (data) {
         this.nextUrl= {searchParams:data};
@@ -23,6 +21,68 @@ describe('Test GET Method to return Solr Query', () =>{
     const requestMockSearch = new FormDataMock({query:value});
     const response = await GET(requestMockSearch, {params});
     expect(response.status).toBe(200)
+  });
+
+});
+
+describe('Test isAcceptedOperator function', () =>{
+  it('should return false', async () => {
+    const input = "fmat"
+    const response = isAcceptedOperator(input);
+    expect(response).toBe(false);
+  });
+
+  it('should return true', async () => {
+    const input = "and"
+    const response = isAcceptedOperator(input);
+    expect(response).toBe(true);
+  });
+
+});
+
+describe('Test generateSimilarWordsQuery function', () => {
+
+  it('should generate a query for a single term', () => {
+    const term = "example";
+    const expectedQuery = 'title:example~1 OR category:example~1 OR metaDescription:example~1';
+    
+    const query = generateSimilarWordsQuery(term);
+    expect(query).toBe(expectedQuery);
+  });
+
+  it('should generate a query for multiple terms', () => {
+    const terms = ["example1", "example2", "example3"];
+    const expectedQuery = 
+      'title:example1~1 OR category:example1~1 OR metaDescription:example1~1 OR ' +
+      'title:example2~1 OR category:example2~1 OR metaDescription:example2~1 OR ' +
+      'title:example3~1 OR category:example3~1 OR metaDescription:example3~1';
+
+    const query = generateSimilarWordsQuery(terms);
+    expect(query).toBe(expectedQuery);
+  });
+
+  it('should generate a query for an empty array', () => {
+    const terms = [];
+    const expectedQuery = '';
+
+    const query = generateSimilarWordsQuery(terms);
+    expect(query).toBe(expectedQuery);
+  });
+
+  it('should handle null input', () => {
+    const terms = null;
+    const expectedQuery = 'title:null~1 OR category:null~1 OR metaDescription:null~1';
+
+    const query = generateSimilarWordsQuery(terms);
+    expect(query).toBe(expectedQuery);
+  });
+
+  it('should handle undefined input', () => {
+    const terms = undefined;
+    const expectedQuery = 'title:undefined~1 OR category:undefined~1 OR metaDescription:undefined~1';
+
+    const query = generateSimilarWordsQuery(terms);
+    expect(query).toBe(expectedQuery);
   });
 
 });
